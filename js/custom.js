d1d3fc6ae157b03afa2d893d9a1998d1b080fbc7
filipkontic3 <filip.kontic3@gmail.com>
2025-11-22 15:27:53 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".navbar").addClass("sticky-nav");
+        } else {
+            $(".navbar").removeClass("sticky-nav");
+        }
+    });
+
+    // BACKSTRETCH SLIDESHOW
+    $('#section_1').backstretch([
+      "images/slide/home1.jpg", 
+      "images/slide/home2.jpg",
+      "images/slide/home3.jpg"
+    ],  {duration: 2000, fade: 750});
+    
+  })(window.jQuery);
+
+
